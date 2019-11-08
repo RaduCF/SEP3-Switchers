98ce1625970b5045b40e00378117428999321cad
Pouneh Samavatian <pooneh2009@gmail.com>
2019-11-08 10:25:16 +0100

    Logic
    changes in class naming + create a the CompareIT class

--- CustomAPI_V4/GoogleClient.cs
+++ CustomAPI_V4/GoogleClient.cs
@@ -1,14 +1,36 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using RestSharp;
 
 namespace CustomAPI_V4
 {
-    partial class Program
+    public class GoogleClient
     {
+        public List<Item> searchApi(string searchitem)
+        {
+            RestClient client =
+                new RestClient(
+                    "https://www.googleapis.com/customsearch/v1?key=AIzaSyC19-EocSwfWeacp7hHZ1L7SQT5ElR4cEg&cx=012558914576125397551:aqn3sajeroi&q=" + searchitem);
+            RestRequest request = new RestRequest(Method.GET);
+
+            var json = client.Execute(request).Content;
+
+            var result = JsonConvert.DeserializeObject<Search>(json);
+            List<Item> resultList = new List<Item>();
+
+            foreach (var items in result.Items)
+            {
+                if (items.Pagemap.Offer != null)
+                {
+                    resultList.Add(items);
+                }
+            }
+            return resultList;
+        }
         //code-maze.com/different-ways-consume-restful-api-csharp/#RestSharp
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             var input = "iphone";
             RestClient client =
@@ -43,8 +65,7 @@ namespace CustomAPI_V4
 
             }
         }
-
-
+        */
     }
 }
 
