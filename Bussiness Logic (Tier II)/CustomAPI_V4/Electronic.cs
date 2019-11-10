90eb44df0df9383e91d7299f4585dcea75cf04bf
TheSweetMarmotte <claire.zubiaurre@gmail.com>
2019-11-10 13:11:47 +0100

    Add a Electronic.cs, ItemElectronic.cs, MainClient.cs

---
+++ Bussiness Logic (Tier II)/CustomAPI_V4/Electronic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CustomAPI_V4
+{
+
+    class Electronic
+    {
+        static HttpClient client = new HttpClient();
+
+        static async Task<SomethingElectronic> GetItemAsync(string searchItem)
+        {
+            string path = "https://localhost:44360/api/Items/" + searchItem;
+            SomethingElectronic item = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                item = await response.Content.ReadAsAsync<SomethingElectronic>();
+            }
+            return item;
+        }
+
+        /*static void Main()
+        {
+            Console.WriteLine("Select a item");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(i);
+            Console.WriteLine("The i selected");
+
+            RunAsync(i).GetAwaiter().GetResult();
+        }*/
+
+        static async Task RunAsync(int i)
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:44360/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                
+                /*string url = "https://localhost:44360/api/Items/" + i.ToString();
+
+                // Get the product
+                SomethingElectronic item = await GetItemAsync(url);
+                getItem(item);*/
+
+                
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        public List<SomethingElectronic> getElectronicList(string searchItem)
+        {
+            List<SomethingElectronic> items = new List<SomethingElectronic>();
+            foreach (var item in items)
+            {
+                items.Add(GetItemAsync(searchItem).Result);
+            }
+            return items;
+        }
+    }
+}
---
+++ Bussiness Logic (Tier II)/CustomAPI_V4/MainClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomAPI_V4
+{
+    class MainClient
+    {
+        
+
+        public static void Main(string[] args)
+        {
+            var gClient = new GoogleClient();
+            var electronic = new Electronic();
+
+            foreach (var Item in gClient.searchApi("iphone"))
+            {
+                Console.WriteLine($"Title= {Item.Title}\nLink={Item.Link}");
+                foreach (var i in Item.Pagemap.Offer)
+                {
+                    Console.WriteLine($"Price= {Item.Pagemap.Offer[0].Price} {Item.Pagemap.Offer[1].Pricecurrency}");
+                }
+            }
+            foreach (var Item in electronic.getElectronicList("iphone"))
+            {
+                Console.WriteLine(Item);
+            }
+        }
+    }
+}
