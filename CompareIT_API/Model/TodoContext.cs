415415d07facc99a24ae21cf0c33bd1982c4ed44
Pouneh Samavatian <pooneh2009@gmail.com>
2019-11-13 00:44:30 +0100

    CompareIT_API
    API between tier 1 and 2 ( not complete yet)

---
+++ CompareIT_API/Model/TodoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CompareIT_API.Model
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<TodoItem> TodoItems { get; set; }
+        }
+    }
+
+
---
+++ CompareIT_API/Model/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompareIT_API.Model
+{
+    public class TodoItem
+    {
+            public long Id { get; set; }
+            public string Title { get; set; }
+            public long Price { get; set; }
+        
+    }
+}
+
