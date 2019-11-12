805f0c8c882744ca294231ccdf373ee8b8bab68c
Radu Candea <43408284+RaduCF@users.noreply.github.com>
2019-11-12 15:58:36 +0100

    Web services client made in tier I

--- WebApplication (Tier I)/Models/Aggregaterating.cs
+++ WebApplication (Tier I)/Models/Aggregaterating.cs
@@ -1,12 +1,14 @@
 ﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace CustomAPI_V4
+namespace Razor_pages_non_mvc.Models
 {
-    /*https://app.quicktype.io/#l=cs&r=json2csharp */
     public class Aggregaterating
     {
         [JsonProperty("ratingvalue")]
         public float Ratingvalue { get; set; }
     }
-
 }
