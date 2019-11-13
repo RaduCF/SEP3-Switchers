33e34d874a5d005e1b9ed77642f219ff0add0a39
Pouneh Samavatian <pooneh2009@gmail.com>
2019-11-13 10:09:02 +0100

    added tier 1

---
+++ WebApplication (Tier I)/Models/Aggregaterating.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_pages_non_mvc.Models
+{
+    public class Aggregaterating
+    {
+        [JsonProperty("ratingvalue")]
+        public float Ratingvalue { get; set; }
+    }
+}
