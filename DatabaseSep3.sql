8d1164aa9ef5eb30966e4f051c008b810dd0085c
AtiBahrani <45685013+AtiBahrani@users.noreply.github.com>
2019-11-20 10:52:00 +0100

    Create DatabaseSep3.sql

---
+++ DatabaseSep3.sql
@@ -0,0 +1,25 @@
+drop schema Public cascade ;
+create schema  Public;
+set schema 'public';
+CREATE TABLE if not exists public."Users"
+(
+
+username character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(9) COLLATE pg_catalog."default" NOT NULL,
+    firstname character varying COLLATE pg_catalog."default",
+    lastname character varying COLLATE pg_catalog."default",
+    email character varying(1) COLLATE pg_catalog."default" NOT NULL,
+    "isAdmin" boolean NOT NULL,
+    CONSTRAINT "Users_pkey" PRIMARY KEY (username)
+);
+drop table public."Users" cascade ;
+CREATE TABLE if not exists public."Wish"
+(
+    "URL" character varying COLLATE pg_catalog."default" NOT NULL,
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Wish_pkey" PRIMARY KEY ("URL"),
+    CONSTRAINT username FOREIGN KEY (username)
+        REFERENCES public."Users" (username) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
